e4cdd7ca993d16737c16d4d4eab38225f513ae9e
Philippe Martin <phmartin@redhat.com>
2025-11-03 07:51:46 +0100

    feat: use table

---
+++ packages/webview/src/component/debugger/columns/NameKind.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+import type { Props } from './props';
+
+let { object }: Props = $props();
+</script>
+
+<div class="flex flex-col max-w-full text-left">
+  <div class="text-(--pd-table-body-text-highlight) overflow-hidden text-ellipsis">
+    {object.name}
+  </div>
+  <div class="text-(--pd-table-body-text) font-extra-light text-sm overflow-hidden text-ellipsis">
+    {object.kind}, {object.type.replace('-', ' ')}
+  </div>
+</div>
