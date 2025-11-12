47d91fc509427354e5ac0873e52620aaaab5bcea
Philippe Martin <phmartin@redhat.com>
2025-11-12 09:17:24 +0100

    feat: step details

--- packages/webview/src/component/debugger/columns/Status.svelte
+++ packages/webview/src/component/debugger/columns/Status.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
-import type { Component } from 'svelte';
+import { getContext, type Component } from 'svelte';
 import type { Props } from './props';
 import { icon } from '/@/component/icons/icon';
 import KubeIcon from '/@/component/icons/KubeIcon.svelte';
 import StarIcon from '/@/component/icons/StarIcon.svelte';
+import { DependencyAccessor } from '/@/inject/dependency-accessor';
+import { DebuggerStepHelper } from '/@/component/debugger/step-helper';
 
 let { object }: Props = $props();
 const Icon: Component = $derived.by(() => icon[object?.kind ?? ''] ?? KubeIcon);
+
+const dependencyAccessor = getContext<DependencyAccessor>(DependencyAccessor);
+const stepHelper = dependencyAccessor.get<DebuggerStepHelper>(DebuggerStepHelper);
 </script>
 
 <div class="grid place-content-center" style="position:relative">
@@ -14,6 +19,7 @@ const Icon: Component = $derived.by(() => icon[object?.kind ?? ''] ?? KubeIcon);
     class="text-(--pd-status-contrast) p-1 grid place-content-center rounded-sm aspect-square text-xs"
     class:bg-(--pd-state-success)={object.type === 'add'}
     class:bg-(--pd-state-error)={object.type === 'delete'}
+    class:bg-(--pd-state-warning)={!stepHelper.isResourceStepUI(object) && object.event.type === 'Warning'}
     role="status"
     title={object.type}>
     <Icon icon={icon} size={20} />
