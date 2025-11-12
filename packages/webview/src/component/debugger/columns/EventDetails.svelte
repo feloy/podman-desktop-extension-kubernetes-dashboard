6df80f465e72020f1ae6a630a31c45a4183ae186
Philippe Martin <phmartin@redhat.com>
2025-11-12 09:17:24 +0100

    feat: step details

--- packages/webview/src/component/debugger/columns/EventDetails.svelte
+++ packages/webview/src/component/debugger/columns/EventDetails.svelte
@@ -1,18 +1,16 @@
 <script lang="ts">
 import type { Props } from './props';
-import type { CoreV1Event, KubernetesObject } from '@kubernetes/client-node';
-import humanizeDuration from 'humanize-duration';
-import moment from 'moment';
+import type { KubernetesObject } from '@kubernetes/client-node';
 import { getManagedField } from '/@/component/debugger/columns/event-details';
+import { getAgeAndCount } from '/@/component/debugger/debugger';
+import { getContext } from 'svelte';
+import { DependencyAccessor } from '/@/inject/dependency-accessor';
+import { DebuggerStepHelper } from '/@/component/debugger/step-helper';
 
 let { object }: Props = $props();
 
-function getAgeAndCount(event: CoreV1Event): string {
-  if ((event.count ?? 0) > 1) {
-    return `${event.count}x over ${humanizeDuration(moment().diff(event.firstTimestamp), { round: true, largest: 1 })} - `;
-  }
-  return '';
-}
+const dependencyAccessor = getContext<DependencyAccessor>(DependencyAccessor);
+const stepHelper = dependencyAccessor.get<DebuggerStepHelper>(DebuggerStepHelper);
 
 function displayGenerationOrResourceVersion(object: KubernetesObject, previous?: KubernetesObject): string {
   if (object.metadata?.generation) {
@@ -31,17 +29,18 @@ function displayGenerationOrResourceVersion(object: KubernetesObject, previous?:
 }
 </script>
 
-{#if ['event-add', 'event-update', 'event-delete'].includes(object.type) && object.event}
+{#if !stepHelper.isResourceStepUI(object) && object.event}
+  {@const ageAndCount = getAgeAndCount(object.event)}
   <div class="flex flex-col max-w-full text-left">
     <div class="text-(--pd-table-body-text-highlight) overflow-hidden text-ellipsis">
-      {getAgeAndCount(object.event)}{object.event.reason}
+      {ageAndCount ? `${ageAndCount} - ` : ''}{object.event.reason}
     </div>
     <div class="text-(--pd-table-body-text) font-extra-light text-sm overflow-hidden text-ellipsis">
       {object.event.message}
     </div>
   </div>
 {/if}
-{#if ['add', 'update', 'delete'].includes(object.type)}
+{#if stepHelper.isResourceStepUI(object)}
   <div class="flex flex-col max-w-full text-left">
     <div class="text-(--pd-table-body-text-highlight) overflow-hidden text-ellipsis">
       {getManagedField(object.object, object.previous)}
