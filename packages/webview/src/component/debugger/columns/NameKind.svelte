133e54c49621e74809f756f45c3c45437fb2b492
Philippe Martin <phmartin@redhat.com>
2025-11-12 09:17:24 +0100

    feat: step details

--- packages/webview/src/component/debugger/columns/NameKind.svelte
+++ packages/webview/src/component/debugger/columns/NameKind.svelte
@@ -1,14 +1,19 @@
 <script lang="ts">
+import { router } from 'tinro';
 import type { Props } from './props';
 
 let { object }: Props = $props();
+
+async function openDetails(): Promise<void> {
+  router.goto(`/debugger/${object.index}/summary`);
+}
 </script>
 
-<div class="flex flex-col max-w-full text-left">
+<button class="flex flex-col max-w-full text-left" onclick={openDetails}>
   <div class="text-(--pd-table-body-text-highlight) overflow-hidden text-ellipsis">
     {object.name}
   </div>
   <div class="text-(--pd-table-body-text) font-extra-light text-sm overflow-hidden text-ellipsis">
     {object.kind}, {object.type.replace('-', ' ')}
   </div>
-</div>
+</button>
--- packages/webview/src/component/debugger/debugger.ts
+++ packages/webview/src/component/debugger/debugger.ts
@@ -16,15 +16,41 @@
  * SPDX-License-Identifier: Apache-2.0
  ***********************************************************************/
 
-import type { CoreV1Event, KubernetesObject } from '@kubernetes/client-node';
+import type { CoreV1Event, KubernetesObject, V1ManagedFieldsEntry } from '@kubernetes/client-node';
+import humanizeDuration from 'humanize-duration';
+import moment from 'moment';
 
-export interface DebuggerStepUI {
-  index: number;
-  selected: boolean;
-  name: string;
-  kind: string;
-  type: 'update' | 'delete' | 'add' | 'event-add' | 'event-update' | 'event-delete';
-  object: KubernetesObject;
-  previous?: KubernetesObject;
-  event?: CoreV1Event;
+export function getAgeAndCount(event: CoreV1Event): string {
+  if ((event.count ?? 0) > 1) {
+    return `${event.count}x over ${humanizeDuration(moment().diff(event.firstTimestamp), { round: true, largest: 1 })}`;
+  }
+  return '';
+}
+
+export function getNewManagedFields(object: KubernetesObject, previous?: KubernetesObject): V1ManagedFieldsEntry[] {
+  let newEntries: V1ManagedFieldsEntry[] = [];
+  if (!previous) {
+    newEntries = object.metadata?.managedFields ?? [];
+  } else {
+    newEntries =
+      object.metadata?.managedFields?.filter(e => !previous.metadata?.managedFields?.some(e2 => entryEqual(e, e2))) ??
+      [];
+  }
+  return newEntries;
+}
+
+function entryEqual(e1: V1ManagedFieldsEntry, e2: V1ManagedFieldsEntry): boolean {
+  return (
+    e1.operation === e2.operation &&
+    e1.manager === e2.manager &&
+    e1.subresource === e2.subresource &&
+    getDateString(e1.time) === getDateString(e2.time)
+  );
+}
+
+function getDateString(date: unknown): string {
+  if (date instanceof Date) {
+    return date.toISOString().replace('.000Z', 'Z');
+  }
+  return date?.toString() ?? '';
 }
