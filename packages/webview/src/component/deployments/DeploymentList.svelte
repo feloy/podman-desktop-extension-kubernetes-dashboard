cd3afa18b7282aa55419fb5f44328cc6f6ad8490
Philippe Martin <phmartin@redhat.com>
2025-11-15 12:27:46 +0100

    feat: add replicasets as children in deployments list
    Signed-off-by: Philippe Martin <phmartin@redhat.com>

--- packages/webview/src/component/deployments/DeploymentList.svelte
+++ packages/webview/src/component/deployments/DeploymentList.svelte
@@ -4,7 +4,7 @@ import moment from 'moment';
 import NameColumn from '/@/component/objects/columns/Name.svelte';
 import StatusColumn from '/@/component/objects/columns/Status.svelte';
 import KubernetesObjectsList from '/@/component/objects/KubernetesObjectsList.svelte';
-import { getContext } from 'svelte';
+import { getContext, onMount } from 'svelte';
 import { DependencyAccessor } from '/@/inject/dependency-accessor';
 import KubernetesEmptyScreen from '/@/component/objects/KubernetesEmptyScreen.svelte';
 import type { DeploymentUI } from './DeploymentUI';
@@ -13,9 +13,14 @@ import DeploymentIcon from '/@/component/icons/DeploymentIcon.svelte';
 import ConditionsColumn from './columns/Conditions.svelte';
 import PodsColumn from './columns/Pods.svelte';
 import ActionsColumn from './columns/Actions.svelte';
+import { ReplicaSetHelper } from '/@/component/replicasets/replicaset-helper';
+import type { ReplicaSetUI } from '/@/component/replicasets/ReplicasetUI';
+import { States } from '/@/state/states';
+import type { ContextResourceItems } from '@kubernetes-dashboard/channels';
 
 const dependencyAccessor = getContext<DependencyAccessor>(DependencyAccessor);
 const deploymenttHelper = dependencyAccessor.get<DeploymentHelper>(DeploymentHelper);
+const replicaSetHelper = dependencyAccessor.get<ReplicaSetHelper>(ReplicaSetHelper);
 
 let statusColumn = new TableColumn<DeploymentUI>('Status', {
   align: 'center',
@@ -54,7 +59,34 @@ const columns = [
   new TableColumn<DeploymentUI>('Actions', { align: 'right', renderer: ActionsColumn }),
 ];
 
-const row = new TableRow<DeploymentUI>({ selectable: (_deployment): boolean => true });
+const states = getContext<States>(States);
+const updateResource = states.stateUpdateResourceInfoUI;
+
+const row = new TableRow<DeploymentUI | ReplicaSetUI>({
+  selectable: (_deployment): boolean => true,
+  children: (deployment): ReplicaSetUI[] => {
+    return (
+      updateResource.data?.resources
+        .filter((resource: ContextResourceItems) => resource.resourceName === 'replicasets')
+        .flatMap(resource =>
+          resource.items
+            .filter(item =>
+              item.metadata?.ownerReferences?.some(
+                owner => owner.kind === 'Deployment' && owner.name === deployment.name,
+              ),
+            )
+            .map(item => replicaSetHelper.getReplicaSetUI(item)),
+        ) ?? []
+    );
+  },
+});
+
+onMount(() => {
+  return updateResource.subscribe({
+    contextName: undefined, // ask for resources in the default context
+    resourceName: 'replicasets',
+  });
+});
 </script>
 
 <KubernetesObjectsList
