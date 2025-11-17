f448d1f08f8e4f41e01dd9b86c76b5844913f96c
Philippe Martin <phmartin@redhat.com>
2025-11-17 15:37:45 +0100

    refactor: ui for steps

--- packages/webview/src/component/debugger/columns/NameKind.svelte
+++ packages/webview/src/component/debugger/columns/NameKind.svelte
@@ -11,9 +11,18 @@ async function openDetails(): Promise<void> {
 
 <button class="flex flex-col max-w-full text-left" onclick={openDetails}>
   <div class="text-(--pd-table-body-text-highlight) overflow-hidden text-ellipsis">
-    {object.name}
+    {#if object.type === 'add'}New {object.kind}
+    {:else if object.type === 'update'}
+      Update {object.kind}
+    {:else if object.type === 'delete'}
+      Delete {object.kind}
+    {:else if object.type === 'event-add'}
+      New event on {object.kind}
+    {:else if object.type === 'event-update'}
+      Update event on {object.kind}
+    {/if}
   </div>
   <div class="text-(--pd-table-body-text) font-extra-light text-sm overflow-hidden text-ellipsis">
-    {object.kind}, {object.type.replace('-', ' ')}
+    {object.name}
   </div>
 </button>
